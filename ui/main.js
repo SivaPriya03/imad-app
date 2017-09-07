6f864033c60c00692efbe754675f360c5a3b8874
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-07 20:48:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML("I am client side javascript");
+var element=document.getElementById('main-text');
+element.innerHTML='I am client side javascript';
