c2fc24887feb651b53b7d93185c9143f2ae693fe
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-07 21:33:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='I am client side javascript';
 var img=document.getElementById('image');
+var marginleft=0;
+function moveRight()
+{
+    marginleft+=10;
+    img.style.marginLeft=marginleft+"px";
+}
 img. onclick=function(){
     var interval=intInterval(moveRight,100);
 };
