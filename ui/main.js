58bdd3b10eafa7c2e6170752ab13f14e0fd9bdaf
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-08 22:47:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ var name=nameip.value;
 var submit=document.getElementById('submitbtn');
 submit.onclick=function()
 {
-    var names={'name1','name2','name3','name4'};
+    var names=['name1','name2','name3','name4'];
     var list='';
     for (var i=0;i<names.length;i++)
     {
