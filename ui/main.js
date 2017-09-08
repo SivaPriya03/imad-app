b7cadb29b503b8bf10627a5bd204c6cc3402ab90
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-08 22:45:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,17 @@ button.onclick=function()
 request.open('GET','http://sivapriya1700.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+var nameip=document.getElementById('name');
+var name=nameip.value;
+var submit=document.getElementById('submitbtn');
+submit.onclick=function()
+{
+    var names={'name1','name2','name3','name4'};
+    var list='';
+    for (var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('list');
+    ul.innerHTML=list;
+};
