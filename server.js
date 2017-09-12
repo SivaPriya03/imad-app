9fb23919f2ab78d1e0f5335d2faa993e9bfdd5c8
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-12 21:42:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,111 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articles=
+{
+
+    'articleOne' :
+    {
+    title: 'Article one | Siva Priya',
+    h1: 'Article One',
+    date : 'Sep 9 2017 :(',
+    content:`
+            <p>
+                The first article describes 
+             Non Programmer description to Git 
+            Here is the link !!
+        </p>
+                <a href="https://blog.scottlowe.org/2015/01/14/non-programmer-git-intro/"> Click here</a>
+             But i can summarise!!!..<br><br>
+                  <h2>Creating Git repository</h2>
+            
+                        <br>
+        <div>To create a new repository, simply create the directory where you want the repository to be housed, then open a command prompt(or terminal window) and navigate to that directory. Once in the directory, use git init to initialize the directory as a Git repository.
+            <br> Cloning a Git repository is super simple: just use the git clone command with a URL<br>
+        </div> `
+    } ,
+    'articleTwo' :
+    {
+        title: 'Article two | Siva Priya',
+        h1: 'Article two',
+        date : 'Sep 10 2017 :(',
+        content:`
+        <p>
+        The second article describes 
+        Non Programmer description to Git 
+        Here is the link !!
+        </p>
+        <a href="https://blog.scottlowe.org/2015/01/14/non-programmer-git-intro/"> Click here</a>
+        But i can summarise!!!..<br><br>
+        <h2>Creating Git repository</h2>
+        
+        <br>
+            <div>To create a new repository, simply create the directory where you want the repository to be housed, then open a command prompt(or terminal window) and navigate to that directory. Once in the directory, use git init to initialize the directory as a Git repository.
+            <br> Cloning a Git repository is super simple: just use the git clone command with a URL<br>
+            </div> `
+        
+    },
+    'articleThree' :
+    {
+            title: 'Article three | Siva Priya',
+            h1: 'Article three',
+            date : 'Sep 11 2017 :(',
+            content:`
+            <p>
+            The third article describes 
+            Non Programmer description to Git 
+            Here is the link !!
+            </p>
+            <a href="https://blog.scottlowe.org/2015/01/14/non-programmer-git-intro/"> Click here</a>
+            But i can summarise!!!..<br><br>
+            <h2>Creating Git repository</h2>
+            
+            <br>
+            <div>To create a new repository, simply create the directory where you want the repository to be housed, then open a command prompt(or terminal window) and navigate to that directory. Once in the directory, use git init to initialize the directory as a Git repository.
+            <br> Cloning a Git repository is super simple: just use the git clone command with a URL<br>
+            </div> `
+        
+    }
+};
+function createTemplate(data)
+{
+    var title=data.title;
+    var h1=data.h1;
+    var date=data.date;
+    var content=data.content;
+    var Template=
+            `<html>
+            <head>
+                <title>
+                   ${title}
+                </title>
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="full">
+                <div>
+                    <a href="/" >Go back to home</a>
+                </div>
+                <hr>
+                <h1>
+                    <i>
+                    ${h1}
+                    </i>
+                </h1>
+                <div>
+                    ${date}        </div>
+                <div>
+                ${content}
+                </div>
+            </body>
+        </html>`;
+return Template;
+
+}
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -46,6 +151,12 @@ app.get('/submit-name', function (req, res) {
    var name= req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName',function(req,res)
+{
+    var articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 
 
