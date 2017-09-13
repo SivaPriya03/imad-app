d983ba5b90c55f187b9c6cf825779a60777bf05f
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-13 23:08:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/', function (req, res) {
 });
 function hash(input)
 {
-   var hashed=crypto.pbkdf2Sync(password,salt,10000,512,'sha512'); 
+   var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512'); 
    return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res)
