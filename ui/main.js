96a55bb2b162c4876d72c3402b0da23251a7530e
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-14 01:13:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -76,7 +76,7 @@ console.log(username);
 console.log(password);
 
 request.open('POST','http://sivapriya1700.imad.hasura-app.io/login',true);
-request.setRequestHeader('Content-Type:application/json');
+request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username,password:password}));
 };
 
