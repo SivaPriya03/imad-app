560ba05e2d7a8de3813d29e04d1e6582d78a9d11
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-14 01:11:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick=function()
 request.open('GET','http://sivapriya1700.imad.hasura-app.io/counter',true);
 request.send(null);
 };
-var submit=document.getElementById('submitbtn');
+var submit=document.getElementById('submitbtn2');
 submit.onclick=function()
 {
     var request =new XMLHttpRequest();
@@ -48,4 +48,36 @@ var myname=nameip.value;
 request.open('GET','http://sivapriya1700.imad.hasura-app.io/submit-name?name=' + myname,true);
 request.send(null);
 };
+
+//Module P11: maintaining session
+submit=document.getElementById('submitbtn');
+submit.onclick=function()
+{
+    var request =new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState ===  XMLHttpRequest.DONE)
+        {
+            if(request.status === 200){
+              console.log("User logged in");
+              alert("Login Success");
+            }
+            else if(request.status===403){
+               console.log("Invalid login");
+               alert("Login fails");
+            }  
+            else
+               alert("Somethong went wrong");
+        }
+    };
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+
+request.open('POST','http://sivapriya1700.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type:application/json');
+request.send(JSON.stringify({username:username,password:password}));
+};
+
     
