7779744f8c95a67e4a08641f40e87b311710f898
SivaPriya03 <30519813+SivaPriya03@users.noreply.github.com>
2017-09-17 13:00:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.post('/create-user',function(req,res)
         if(err)
           res.status(500).send(err.toString());
         else
-          res.send("User Successfully created "+username);
+          res.status(200).send("User Successfully created "+username);
     });
 })
 app.post('/login',function(req,res)
